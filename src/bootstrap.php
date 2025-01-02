6dc7ae2c6cf1caef17061a142d4af1ae1f70d39a
Bartlomiej Wilczynski <bartek.wilczynski@icloud.com>
2025-01-02 19:45:57 +0100

    Include the correct namespace and initial Twig setup

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-use CMS\CMS;
-
 define("APP_ROOT", dirname(__FILE__, 2)); // Ścieżka do katalogu projektu w moim przypadku: C:\xampp\htdocs\creative-folk
 require APP_ROOT . '/src/functions.php';
 require APP_ROOT . '/config/config.php';
@@ -14,12 +12,18 @@ if (DEV === false) {
     register_shutdown_function('shutdown_handler');  // Procedura zamykająca
 }
 
-spl_autoload_register(function ($class) {
-    $path = APP_ROOT . '/src/classes/';
-    require $path . $class . '.php';
-});
-
-$cms = new CMS($dsn, $db_user, $db_password);
+$cms = new \PhpMysql\CMS\CMS($dsn, $db_user, $db_password);
 unset($dsn, $db_user, $db_password);
+
+$twig_options['cache'] = APP_ROOT . '/var/cache';
+$twig_options['debug'] = DEV;
+
+$loader = new Twig\Loader\FilesystemLoader(APP_ROOT . '/templates');
+$twig = new Twig\Environment($loader, $twig_options);
+$twig->addGlobal('document_root', DOCUMENT_ROOT);
+
+if (DEV === true) {
+    $twig->addExtension(new \Twig\Extension\DebugExtension());
+}
 
 $navbar_categories = $cms->categories()->getAll();
