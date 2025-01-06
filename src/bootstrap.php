5f76ef02b7c01c0972b07ee5ab2096043cfa3708
Bartlomiej Wilczynski <bartek.wilczynski@icloud.com>
2025-01-06 18:20:34 +0100

    Remove head_tags array

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,6 @@
 define("APP_ROOT", dirname(__FILE__, 2)); // Ścieżka do katalogu projektu w moim przypadku: C:\xampp\htdocs\creative-folk
 require APP_ROOT . '/src/functions.php';
 require APP_ROOT . '/config/config.php';
-require APP_ROOT . "/src/metatags.php";
 require APP_ROOT . "/vendor/autoload.php"; // Zastępuje funkcje spl_autoload_register
 
 if (DEV === false) {
--- src/metatags.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Domyślne meta tagi
-$head_tags = [
-    'title' => "Domyślny tytuł strony",
-    'description' => "Domyślny opis strony",
-    'keywords' => "creative-folk, creative, folk",
-];
