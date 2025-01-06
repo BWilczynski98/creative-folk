238d1c6dedcb0855fa67cc3c8f74eae5dcd2721d
Bartlomiej Wilczynski <bartek.wilczynski@icloud.com>
2025-01-06 18:08:28 +0100

    Remove a photo assigned to a post

--- public/admin/delete-publication.php
+++ public/admin/delete-publication.php
@@ -1,33 +1,32 @@
 <?php
 declare(strict_types=1);
 include "../../src/bootstrap.php";
-// TODO: Kiedy usuwa się publikacje to nie usuwa się zdjęcie z bazy danych
+
 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 $publication = [];
 
-// Walidacja id (Puste i nie w typie int)
 if (!$id) {
     redirect('publications.php', ['error' => 'Nie odnaleziono artykułu']);
 }
 
-// Id nie jest puste i jest w typie int, próba pobrania kategorii
 if ($id) {
     $publication = $cms->publications()->getById($id, false);
 }
 
-// Nie odnaleziono kategorii z podanym id
 if (!$publication) {
     redirect('publications.php', ['error' => 'Nie odnaleziono artykułu']);
 }
 
-// Wywołanie dla przycisku "Potwierdź"
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($publication) {
-        $response = $cms->publications()->delete($publication['id']);
-        redirect('publications.php', ['success' => 'Publikacja została poprawnie usunięta']);
-    } else {
-        redirect('publications.php', ['error' => 'Nie odnaleziono artykułu']);
+    if (isset($publication['image_file'])) {
+        $file_path = APP_ROOT . '/public/sent/' . $publication['image_file'];
+        $cms->publications()->deleteImage($publication['id_image'], $file_path, $publication['id']);
     }
+
+    $response = $cms->publications()->delete($publication['id']);
+    $response ? redirect('publications.php', ['success' => 'Publikacja została poprawnie usunięta']) :
+                redirect('publications.php', ['error' => 'Nie odnaleziono artykułu']);
+
 }
 
 $data = [
