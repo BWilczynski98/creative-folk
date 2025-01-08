84cd2d32b776d335ceac7a722631dce1b3e37cb7
Bartlomiej Wilczynski <bartek.wilczynski@icloud.com>
2025-01-08 15:18:57 +0100

    Test for passwords

--- src/classes/Validation/Validator.php
+++ src/classes/Validation/Validator.php
@@ -9,4 +9,15 @@ class Validator
         return (strlen($string) >= $min && strlen($string) <= $max);
     }
 
+    public static function password(string $password): bool {
+        if (mb_strlen($password) >= 8 &&
+            preg_match('/[A-Z]/', $password) &&
+            preg_match('/[a-z]/', $password) &&
+            preg_match('/[0-9]/', $password)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
 }
